ae85b95f6faafdc60a7f2a15c579cca72db30908
loganbresnahan <loganbbres@gmail.com>
2016-03-21 20:13:59 -0500

    Add schema picture and finish user model.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+   validates :username, presence: true
+   validates :email, presence: true, uniqueness: true
+   has_secure_password
+
+   has_many :notes
+   has_many :videos, through: :notes
+   has_many :timestamps, through: :notes
+end
