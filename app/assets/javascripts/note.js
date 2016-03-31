4c50823ea5b2daf38f3ae8d133f4066400dc1ee1
loganbresnahan <loganbbres@gmail.com>
2016-03-31 15:57:14 -0500

    Update css textwrap for all browsers.

---
+++ app/assets/javascripts/note.js
@@ -0,0 +1,15 @@
+// $('.edit-form').on('click', '#note-edit-form', function(event){
+//   // event.preventDefault();
+//   console.log($(this));
+//
+//   var request = $.ajax({
+//     url: $(this).attr('url'),
+//     method: 'post',
+//     dataType: JSONP,
+//     data: $(this).serialize()
+//   });
+//
+//   request.done(function(response){
+//       $('#note-title').replaceWith(response);
+//   });
+// });
