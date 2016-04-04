e797eddebb6dc3ce1f7bb48da462d19b59856206
loganbresnahan <loganbbres@gmail.com>
2016-04-04 16:26:13 -0500

    Add classes to forms and show page and youtube."

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,9 @@ module YoutubeNoteTaker
   end
 end
 
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
+# config.before_configuration do
+#   env_file = File.join(Rails.root, 'config', 'local_env.yml')
+#   YAML.load(File.open(env_file)).each do |key, value|
+#     ENV[key.to_s] = value
+#   end if File.exists?(env_file)
+# end
