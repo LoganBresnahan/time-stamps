985914caa93d7a0f78fe63db2f40044f638127b1
loganbresnahan <loganbbres@gmail.com>
2016-04-08 10:32:17 -0500

    Write and add ruby sort time helper to stamp show.

--- app/helpers/sort_time_helper.rb
+++ app/helpers/sort_time_helper.rb
@@ -1,15 +1,15 @@
 module SortTimeHelper
 
   def seconds_converter(formated_time)
-    minutes = formated_time.match(/\#([0-9]*)/)[1]
-    seconds = formated_time.match(/\:([1-9]*)/)[1]
+    minutes = formated_time.match(/([0-9]*)/)[0]
+    p seconds = formated_time.match(/\:([0-9]*)/)[1]
     return (minutes.to_i * 60) + seconds.to_i
   end
 
   def sort_time(array)
-    array.sort_by do |time|
-      seconds_converter(time)
+    array.sort_by do |stamp|
+      seconds_converter(stamp.time)
     end
   end
-  
+
 end
