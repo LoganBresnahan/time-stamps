d060f4da8da2970ba97d6f0bfcee6736f084c5cc
loganbresnahan <loganbbres@gmail.com>
2016-04-08 11:32:39 -0500

    Update sort method to handle custom character times.

--- app/helpers/sort_time_helper.rb
+++ app/helpers/sort_time_helper.rb
@@ -1,9 +1,13 @@
 module SortTimeHelper
 
   def seconds_converter(formated_time)
-    minutes = formated_time.match(/([0-9]*)/)[0]
-    p seconds = formated_time.match(/\:([0-9]*)/)[1]
-    return (minutes.to_i * 60) + seconds.to_i
+    if formated_time.match(/([0-9]*):([0-9]*)/)
+      minutes = formated_time.match(/([0-9]*)/)[0]
+      p seconds = formated_time.match(/\:([0-9]*)/)[1]
+      return (minutes.to_i * 60) + seconds.to_i
+    else
+      return formated_time = 0
+    end
   end
 
   def sort_time(array)
